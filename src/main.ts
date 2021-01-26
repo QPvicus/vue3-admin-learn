08f9334e870c0b293eb9f175a02b5c7e0a3fdff3
QPvicus <57519939+QPvicus@users.noreply.github.com>
2021-01-26 15:34:15 +0800

    '配置文件'

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import App from "./App";
 import router from "./router";
 import store from "./store";
-
-createApp(App)
+import ElementPlus from "element-plus";
+import "element-plus/lib/theme-chalk/index.css";
+const app = createApp(App);
+app
+  .use(ElementPlus)
   .use(store)
   .use(router)
   .mount("#app");
